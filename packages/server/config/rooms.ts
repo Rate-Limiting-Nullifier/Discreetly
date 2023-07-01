6a8a2ffadcdd97fe341a51ddeaeb202b23364146
AtHeartEngineer <1675654+AtHeartEngineer@users.noreply.github.com>
2023-06-30 20:19:11 -0400

    Server Config setup

---
+++ packages/server/config/rooms.ts
@@ -0,0 +1,64 @@
+import { MembershipI, RoomGroupI, ServerI } from '../../protocol-interfaces/src/main';
+import { genId } from '../src/utils';
+import 'dotenv/config';
+
+let serverID = 999n;
+
+try {
+  serverID = process.env.serverID ? (process.env.serverID as unknown as bigint) : 999n;
+} catch (error) {
+  console.error('Error reading serverID from .env file!');
+}
+console.log('SERVERID:', serverID);
+
+const idcommitment_1 = genId(serverID, 0n);
+const idcommitment_2 = genId(serverID, 1n);
+const idcommitment_3 = genId(serverID, 2n);
+const idcommitment_4 = genId(serverID, 3n);
+const idcommitment_5 = genId(serverID, 4n);
+
+export const rooms: RoomGroupI[] = [
+  {
+    name: 'Discreetly',
+    rooms: [
+      {
+        id: genId(serverID, 'General'),
+        name: 'General',
+        membership: [idcommitment_1, idcommitment_2, idcommitment_5] as MembershipI
+      },
+      {
+        id: genId(serverID, '1EthRoom'),
+        name: '1EthRoom',
+        membership: [idcommitment_1, idcommitment_2, idcommitment_5] as MembershipI
+      }
+    ]
+  },
+  {
+    name: 'Games',
+    rooms: [
+      {
+        id: genId(serverID, 'Gunfire Reborn'),
+        name: 'Gunfire Reborn',
+        membership: [idcommitment_2, idcommitment_3, idcommitment_4] as MembershipI
+      }
+    ]
+  },
+  {
+    name: 'Events',
+    rooms: [
+      {
+        id: genId(serverID, 'Devconnect 2023'),
+        name: 'Devconnect 2023',
+        membership: [idcommitment_1, idcommitment_3, idcommitment_5] as MembershipI
+      }
+    ]
+  }
+];
+
+export const serverConfig: ServerI = {
+  name: 'Discreetly',
+  serverInfoEndpoint: 'localhost:3001',
+  messageHandlerSocket: 'localhost:3002',
+  version: '0.0.1',
+  roomGroups: rooms
+};
--- packages/server/src/utils.ts
+++ packages/server/src/utils.ts
@@ -1,5 +1,5 @@
 import { poseidon2 } from 'poseidon-lite/poseidon2';
 
-export function genRoomId(roomName: string) {
-  return poseidon2([this.serverID, BigInt(roomName)]);
+export function genId(serverID: bigint, roomName: string | bigint | number) {
+  return poseidon2([serverID, BigInt(roomName)]);
 }
