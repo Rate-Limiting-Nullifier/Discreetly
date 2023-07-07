6dcc7633b6d57ed2da8af3f6e20672420f75d6c0
AtHeartEngineer <1675654+AtHeartEngineer@users.noreply.github.com>
2023-07-06 22:48:25 -0400

    server todos

--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -3,7 +3,7 @@ import { Server } from 'http';
 import { Server as SocketIOServer, Socket } from 'socket.io';
 import * as cors from 'cors';
 // import { createClient } from 'redis';
-// import initializeClaimCodeManager from './inviteCodes';
+import initializeClaimCodeManager from './inviteCodes';
 import { serverConfig, rooms } from '../config/rooms';
 import { MessageI } from '../../protocol-interfaces/src/main';
 import verifyProof from './verifier';
@@ -33,13 +33,14 @@ interface userCountI {
 
 let userCount: userCountI = {};
 
+// TODO get the claim code manager working with redis to store the state of the rooms and claim codes in a redis database that persists across server restarts
 // Redis
 
 // const redisClient = createClient();
 // redisClient.on('error', (err) => console.log('Redis Client Error', err));
 // await redisClient.connect();
 
-// const ccm = await initializeClaimCodeManager(redisClient);
+//const ccm = await initializeClaimCodeManager();
 
 io.on('connection', (socket: Socket) => {
   console.debug('a user connected');
@@ -86,13 +87,17 @@ app.get('/api/rooms', (req, res) => {
   res.json(rooms);
 });
 
-// app.post('/join', (req, res) => {
-//   req.on('data', (data) => {
-//     if (ccm.claimCode(data.claimCode)) {
-//       res.status(200).send('OK');
-//     }
-//   });
-// });
+app.post('/join', (req, res) => {
+  req.on('data', (data) => {
+    // TODO This is where we would validate the claim/invite code
+    // TODO the `result` is in this format: https://github.com/AtHeartEngineering/Discreetly/blob/f2ea89d4b87004693985854e17a4e669177c4df3/packages/claimCodes/src/manager.ts#L10
+    //const result = ccm.claimCode(data.claimCode);
+    // TODO The `groupID` is the room ID like in https://github.com/AtHeartEngineering/Discreetly/blob/acc670fc4c43aa545dbbd03817879abfe5bc819e/packages/server/config/rooms.ts#L37
+    // TODO If the claim code is valid, then we would add the user to the room
+    // const identityCommitment = data.identityCommitment; // FIX this is the identity commitment from the user, think of it as a user ID
+    res.status(200).send('OK');
+  });
+});
 
 app.listen(http_port, () => {
   console.log(`Http Server is running at http://localhost:${http_port}`);
