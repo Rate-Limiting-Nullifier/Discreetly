f7f27dbc14b3e103336edd132c9b11120e2909f8
Tanner Shaw <traumaaa@gmail.com>
2023-07-07 17:58:36 -0500

    redis distribution for claim codes and rooms

--- packages/server/src/inviteCodes.ts
+++ packages/server/src/inviteCodes.ts
@@ -1,21 +1,22 @@
 import ClaimCodeManager from '../../claimCodes/src/manager';
 
-const initializeClaimCodeManager = async (redisClient) => {
-  let claimCodeSets = await redisClient.get('ccm');
+const initializeClaimCodeManager = (redisClient) => {
   let ccm: ClaimCodeManager;
-  if (!claimCodeSets) {
-    ccm = new ClaimCodeManager();
-    ccm.generateClaimCodeSet(10, 999, 'TEST');
-    const ccs = ccm.getClaimCodeSets();
-    await redisClient.set('ccm', JSON.stringify(ccs));
-  } else {
-    ccm = new ClaimCodeManager(JSON.parse(claimCodeSets));
-    if (ccm.getUsedCount(999).unusedCount < 5) {
+  redisClient.get('ccm').then(cc => {
+    if (!cc) {
+      ccm = new ClaimCodeManager();
       ccm.generateClaimCodeSet(10, 999, 'TEST');
       const ccs = ccm.getClaimCodeSets();
-      await redisClient.set('ccm', JSON.stringify(ccs));
+      redisClient.set('ccm', JSON.stringify(ccs));
+    } else {
+      ccm = new ClaimCodeManager(JSON.parse(cc));
+      if (ccm.getUsedCount(999).unusedCount < 5) {
+        ccm.generateClaimCodeSet(10, 999, 'TEST');
+        const ccs = ccm.getClaimCodeSets();
+        redisClient.set('ccm', JSON.stringify(ccs));
+      }
     }
-  }
+  });
   return ccm;
 };
 
