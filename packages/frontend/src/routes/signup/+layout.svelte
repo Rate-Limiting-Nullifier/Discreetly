bdb1365ef040ce466ce1a7f077363d7989bbf5ad
AtHeartEngineer <1675654+AtHeartEngineer@users.noreply.github.com>
2023-07-08 10:54:30 -0400

    signup progress tracking

--- packages/frontend/src/routes/signup/+layout.svelte
+++ packages/frontend/src/routes/signup/+layout.svelte
@@ -7,6 +7,7 @@
 		<a
 			class="nav-link"
 			class:active={!$page.route.id?.match(/gates|ceremony|backup/g)}
+			class:complete={$page.route.id?.match(/gates|ceremony|backup/g)}
 			aria-current="page"
 			href="/signup">Sign Up</a
 		>
@@ -16,14 +17,14 @@
 			class="nav-link"
 			href="/signup/gates"
 			class:active={$page.route.id?.match(/gates/g)}
-			class:step-primary={$page.route.id?.match(/gates|ceremony|backup/g)}>Choose Communities</a
+			class:complete={$page.route.id?.match(/ceremony|backup/g)}>Choose Communities</a
 		>
 	</li>
 	<li class="nav-item">
 		<a
 			class="nav-link"
 			href="/signup/ceremony"
-			class:step-primary={$page.route.id?.match(/ceremony|backup/g)}
+			class:complete={$page.route.id?.match(/backup/g)}
 			class:active={$page.route.id?.match(/ceremony/g)}>Create Identity</a
 		>
 	</li>
@@ -39,3 +40,10 @@
 		<slot />
 	</div>
 </div>
+
+<style>
+	.complete::after {
+		content: '✓';
+		padding-left: 0.5rem;
+	}
+</style>
